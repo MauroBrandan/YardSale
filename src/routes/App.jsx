b3fd7f8c09e15b5c30d84794b43dce10f8ebf279
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-08-26 22:22:05 -0300

    Rutas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import '../styles/global.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from '../containers/Layout'
-import { Navbar } from '../components/Navbar'
-import { Login } from '../containers/Login.jsx'
-import { Signup } from '../containers/Signup'
+import Layout from '../containers/Layout';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import PasswordRecovery from '../pages/PasswordRecovery';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
+import NotFound from '../pages/NotFound';
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Layout>
-				<Navbar />
 				<Routes>
-					<Route path='/' element={<Login />} />
-					<Route path='signup' element={<Signup />} />
+					<Route path='/' element={<Home />} />
+					<Route path='login' element={<Login />} />
+					<Route path='password-recovery' element={<PasswordRecovery />} />
+					<Route path='send-email' element={<SendEmail />} />
+					<Route path='new-password' element={<NewPassword />} />
+					<Route path='account' element={<MyAccount />} />
+					<Route path='signup' element={<CreateAccount />} />
+					<Route path='checkout' element={<Checkout />} />
+					<Route path='orders' element={<Orders />} />
+					<Route path='*' element={<NotFound />} />
 				</Routes>
 			</Layout>
 		</BrowserRouter>
