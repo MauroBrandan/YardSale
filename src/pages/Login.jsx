175d89e71f26060c54ede1268681e3309e165d54
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-08-29 22:27:05 -0300

    Obtencion de datos del formulario de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../styles/Login.css'
 import Logo from '../assets/logos/logo_yard_sale.svg'
 
 const Login = () => {
+	const form = useRef(null)
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		const formData = new FormData(form.current)
+		const data = {
+			username: formData.get('email'),
+			password: formData.get('password'),
+		}
+
+		console.log(data)
+	}
+
 	return (
 		<div className='Login'>
 			<div className='Login-container'>
 				<img src={Logo} alt='logo' className='logo' />
-				<form action='/' className='form'>
+				<form action='/' className='form' ref={form}>
 					<label htmlFor='email' className='label'>
 						Email address
 					</label>
 					<input
 						type='text'
-						id='email'
+						name='email'
 						placeholder='email@example.cm'
 						className='input input-email'
 					/>
@@ -22,7 +35,7 @@ const Login = () => {
 					</label>
 					<input
 						type='password'
-						id='password'
+						name='password'
 						placeholder='*****'
 						className='input input-password'
 					/>
@@ -30,6 +43,7 @@ const Login = () => {
 						type='submit'
 						value='Log in'
 						className='primary-button login-button'
+						onClick={handleSubmit}
 					/>
 					<a href='/'>Forgot my password</a>
 				</form>
